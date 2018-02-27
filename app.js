2ea9107be31bc0ed9a4a75b773de93cc0f19bd89
Andres Lopez <andres.lopez@banlinea.com>
2018-02-26 21:22:57 -0500

    publish complete app

--- app.js
+++ app.js
@@ -1,15 +1,105 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express= require('express'),
+    bodyParser = require('body-parser'),
+    http = require('http'),
+    app,
+    router,
+    server;
 
 var consumeApi = require('./public/Controllers/consumeLolApi.js');
 
-var ChampionC = require('./public/Models/ChampionMaestries');
-var SummonerC = require('./public/Models/Summoner');
 
-var champList = Array<ChampionC.ChampionMaestries>[];
-var summoner = SummonerC.Summoner;
+
+app = express();
+app.use(bodyParser.json());
+
+router = express.Router();
+
+router.get('/champMaestries/:userid',(req,res)=>{
+    var userid = req.params.userid;
+
+    consumeApi.getChampMaestries(userid,(err,body)=>{
+        if(err){
+            console.log(err);
+        }else{
+            var response = body;
+            let champList = JSON.parse(response);
+
+            res.json(champList);
+            console.log("entramos al championList")
+            console.log(champList);
+        }
+        
+    })
+});
+
+router.get('/summoner/:username',(req,res)=>{
+    var username = req.params.username;
+
+    consumeApi.getPlayerByName(username,(err,body)=>{
+        if(err){
+            console.log(err);
+        }else{
+            var response = body;
+            var summoner = JSON.parse(response);
+            res.json(summoner);
+            console.log("consulta al getsummonername " + username);
+        }
+    });
+    console.log(username);
+});
+
+router.get('/champ/:id',(req,res)=>{
+    var champid = req.params.id;
+    consumeApi.getChampDetailsById(champid,(err,body)=>{
+        if(err){
+            console.log(err);
+        }else{
+            var response = body;
+            var champDetail = JSON.parse(response);
+            res.json(champDetail);
+            console.log("Champion Detail By Id" + champid);
+        }
+    });
+});
+
+router.get('/champjson/:name',(req,res)=>{
+    var name = req.params.name;
+    console.log(name);
+    consumeApi.getChampDetailsByNameJson(name,(err,body)=>{
+        if(err){
+            console.log(err);
+        }else{
+            var response = body;
+            var champDetail = JSON.parse(response);
+
+            //console.log(testChamp.MonkeyKing);
+            // console.log(name);
+            res.json(champDetail);
+        }
+    });
+});
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('User-Agent', 'javascript');
+    next();
+});
+
+app.use('/api',router);
+
+server = http.createServer(app);
+server.listen(3000,()=>{
+    console.log("arrancamos el server");
+});
+
+
+// var ChampionC = require('./public/Models/ChampionMaestries');
+// var SummonerC = require('./public/Models/Summoner');
+
+// var champList = Array<ChampionC.ChampionMaestries>[];
+// var summoner = SummonerC.Summoner;
 
 // let listchamps = consumeApi.getChampMaestries(202063,function(err, body){
 //     if(err){
@@ -23,35 +113,18 @@ var summoner = SummonerC.Summoner;
 //     }
 // });
 
-function getSummoner(username){
+// function getSummoner(username){
     
-}
+// }
 
-app.get('/', (req,res)=>{
-    res.sendfile(__dirname + '/public/Views/index.html')
-});
+// app.get('/', (req,res)=>{
+//     res.sendfile(__dirname + '/public/Views/index.html')
+// });
 
-app.use('/public',express.static(__dirname + '/public'));
 
-http.listen(process.env.PORT || 3000,()=>{
-    console.log('listen on port: 3000');
-});
 
-io.on('connection',(socket)=>{
-    console.log("connected");
-    
-    socket.on('getUser',(data)=>{
-        
-        console.log('llego a getUser de lado del servidor');
-        consumeApi.getPlayerByName(data,(err,body)=>{
-            if(err){
-                console.log(err);
-            }else{
-                var response = body;
-                let summoner = JSON.parse(response);
-                socket.emit('getName',summoner);
-                // console.log(summoner.name);
-            }
-        });
-    });
-});
+// app.use('/public',express.static(__dirname + '/public'));
+
+// http.listen(process.env.PORT || 3000,()=>{
+//     console.log('listen on port: 3000');
+// });
--- public/Controllers/consumeLolApi.js
+++ public/Controllers/consumeLolApi.js
@@ -1,13 +1,52 @@
 var request = require('request');
 let url = 'https://la1.api.riotgames.com/lol/';
+const KEY = 'RGAPI-5e8f5202-11bc-4a02-8379-1b03adff7388';
+var fs = require('fs');
+var jsonquery = require('json-query');
+
+
+var champjson = fs.readFileSync('public/Controllers/champions.json','utf-8');
+var jsondetails = fs.readFileSync('public/Controllers/championsdetails.json','utf-8');
 
 
 function createUrl(model){
     return url + model;
 }
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+module.exports.getChampDetailsByNameJson = (champname,callback)=>{
+    if(champname){
+        if(champname === "all" ){
+            callback(null,champjson);
+        }
+        champname = toTitleCase(champname);
+        let json = JSON.parse(jsondetails);
+        let champdetails = JSON.parse(champjson);
+        let result = jsonquery('data[aliasname='+champname+'].name',{
+            data:json
+        });    
+        if(result.value){
+    // console.log(jsondetails);
+    //console.log(Object.keys(json.data));
+
+            console.log(champdetails.data[result.value].name);
+            callback(null,JSON.stringify(champdetails.data[result.value]));
+        }else{
+            callback({statusCode: 404});
+        }
+    
+    }else{
+        callback({statusCode:404})
+    }
+
+    
+}
 
 module.exports.getChampMaestries = (userid,callback)=>{
-    let url = createUrl('champion-mastery/v3/champion-masteries/by-summoner/'+userid+'?api_key=RGAPI-30605210-5be9-4d2b-917d-3f790bd2be37');
+    let url = createUrl('champion-mastery/v3/champion-masteries/by-summoner/'+userid+'?api_key='+KEY);
     var result = Array();
     request({
         url:url,
@@ -21,7 +60,7 @@ module.exports.getChampMaestries = (userid,callback)=>{
 }
 
 module.exports.getPlayerByName = (username,callback)=>{
-    let url = createUrl('summoner/v3/summoners/by-name/'+username+'?api_key=RGAPI-30605210-5be9-4d2b-917d-3f790bd2be37');
+    let url = createUrl('summoner/v3/summoners/by-name/'+username+'?api_key='+KEY);
     var result = Array();
     request({
         url:url,
@@ -30,6 +69,26 @@ module.exports.getPlayerByName = (username,callback)=>{
         if(error || response.statusCode !== 200){
             return callback(error || {statusCode: response.statusCode});
         }
-        callback(null,JSON.stringify(body));
+        let result = {
+            id: body.id,
+            name:body.name,
+            imageProfile: body.profileIconId,
+            level: body.summonerLevel
+        };
+        callback(null,JSON.stringify(result));
     });
+}
+
+module.exports.getChampDetailsById = (champid,callback)=>{
+    let url = createUrl('static-data/v3/champions/'+champid+'?locale=es_MX&tags=image&api_key='+KEY)
+    var result = Array();
+    request({
+        url:url,
+        json:true
+    },(err,res,body)=>{
+        if(err){
+            return callback(err || {statusCode:res.statusCode});
+        }
+        callback(null,JSON.stringify(body));
+    })
 }
